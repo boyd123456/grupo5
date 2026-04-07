57ed5a8262d35044055c55162dd3ca3a28584b8f
dixlixedu-AOD <dixlixedu@gmail.com>
2026-04-06 23:20:14 -0500

    Atributos Usuarios

--- PlataExpress/Models/Usuarios.cs
+++ PlataExpress/Models/Usuarios.cs
@@ -1,6 +1,10 @@
 ﻿namespace PlataExpress.Models
 {
     public class Usuarios
+
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
