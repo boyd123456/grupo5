45c3bbc0b68ea04b9bc3a171ab73166582f8f520
boyd <167488040+boyd123456@users.noreply.github.com>
2026-04-08 16:57:39 -0500

    CREACION DE LA BASE DETAOS   Y TABLAS    Y LA CONEXION

--- PlataExpress/Models/anotacionBaseDeDatos.cs
+++ PlataExpress/Models/anotacionBaseDeDatos.cs
@@ -2,5 +2,33 @@
 {
     public class anotacionBaseDeDatos
     {
+
+
+        /*CREATE TABLE [dbo].[Usuarios]
+(
+	[IdUsuario] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Nombres] VARCHAR(100) NOT NULL, 
+    [Correo] VARCHAR(100) NOT NULL, 
+    [Usuario] VARCHAR(100) NOT NULL, 
+    [Clave] VARCHAR(100) NOT NULL, 
+    [Telefono] VARCHAR(20) NOT NULL, 
+    [Dni] VARCHAR(8) NOT NULL, 
+    [Rol] VARCHAR(50) NOT NULL, 
+    [FechaRegistro] DATETIME NOT NULL DEFAULT GETDATE()
+
+
+
+
+        ------------------------------------
+
+        CONEXION DE LA BASE DE DATOS
+
+
+        "ConnectionStrings": {
+    "DineroConnection": "Server=(localdb)\\MSSQLLocalDB;Database=BDplataExpress;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true"
+  
+},
+)
+*/
     }
 }
